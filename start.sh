b39c15363d9499dfe29b1a5d4b23c6611b81e64c
Praveen Kumar K <125640021+shinchan2627@users.noreply.github.com>
2024-03-05 12:31:01 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,27 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/lazyindu/test1.git /LazyPrincess
+  git clone https://github.com/shinchan2627/Master-Roshi-Bot-V2.git /Master-Roshi-Bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /LazyPrincess
+  git clone $UPSTREAM_REPO /Master-Roshi-Bot
 fi
 cd /LazyPrincess
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
-
-UPSTREAM_REPO="https://github.com/shinchan2627/Master-Roshi-Bot-V2.git"
-
-# if [ -z $UPSTREAM_REPO ]
-# then
-#   echo "Cloning main Repository"
-#   git clone https://github.com/lazyindu/test1.git /LazyPrincess
-# else
-#   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-#   git clone $UPSTREAM_REPO /LazyPrincess
-# fi
-# cd /LazyPrincess
-# pip3 install -U -r requirements.txt
-# echo "Starting Bot...."
-# python3 bot.py
